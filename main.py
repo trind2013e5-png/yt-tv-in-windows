abc9e1ac741170b3f68212438bc649fd992dc6a9
Duc Tri <trind2013e5@gmail.com>
2026-05-12 19:59:45 +0700

    Refactor Smart TV browser application setup

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import subprocess
 
 # --- TỰ ĐỘNG CÀI THƯ VIỆN ---
 def install_dependencies():
-    try:
-        import PyQt5
-        import PyQt5.QtWebEngineWidgets
-    except ImportError:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "PyQtWebEngine"])
+ try:
+ import PyQt5
+ import PyQt5.QtWebEngineWidgets
+ except ImportError:
+ subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "PyQtWebEngine"])
 
 install_dependencies()
+
+# --- CHƯƠNG TRÌNH CHÍNH ---
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
+# Lớp trình duyệt tùy chỉnh để chặn menu chuột phải
 class CustomWebView(QWebEngineView):
  def contextMenuEvent(self, event):
+ # Không làm gì cả (không gọi hàm gốc) để menu không hiện ra
  pass
 
 class ClickFilter(QObject):
@@ -27,16 +32,22 @@ class ClickFilter(QObject):
 class SmartTVBrowser(QMainWindow):
  def __init__(self):
  super(SmartTVBrowser, self).__init__()
+
+ # 1. User Agent Smart TV
  self.user_agent = "Mozilla/5.0 (SMART-TV; Linux; Tizen 5.0) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/2.2 Chrome/63.0.3239.84 TV Safari/537.36"
  self.profile = QWebEngineProfile.defaultProfile()
  self.profile.setHttpUserAgent(self.user_agent)
+
+ # 2. Sử dụng CustomWebView đã chặn chuột phải
  self.browser = CustomWebView()
  self.browser.setUrl(QUrl('https://www.youtube.com/tv')) 
+ 
+ # 3. Chặn chuột trái
  self.filter = ClickFilter()
  self.browser.focusProxy().installEventFilter(self.filter)
 
  self.setCentralWidget(self.browser)
- self.setWindowTitle("Smart TV")
+ self.setWindowTitle("Python Smart TV - No Menu - No Left Click")
  self.showMaximized()
 
 if __name__ == "__main__":
